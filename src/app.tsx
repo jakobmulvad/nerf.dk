514c081221d481ccb7572427efb411658f729aba
Jakob Mulvad Nielsen <mulvad@gmail.com>
2019-11-06 22:54:31 +0100

    Add simple logo for now

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App : React.FC = () => <div>hello world!</div>
+const App : React.FC = () => {
+	return <div className="App-header">
+		<span className="App-logo">nerf</span>
+	</div>
+}
 
 render(<App/>, document.getElementById('root'));
