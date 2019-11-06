342f5707fd68efde5a019eea129ad4cb7410b1d6
Jakob Mulvad Nielsen <mulvad@gmail.com>
2019-11-06 09:25:44 +0100

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App : React.FC = () => <div>hello world!</div>
+
+render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.tsx',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/, }
+    ]
+  }
+};
