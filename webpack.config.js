a3f796b32f6e2960618bb0f0892f4b9c3e1f1543
Jakob Mulvad Nielsen <mulvad@gmail.com>
2019-11-07 15:12:35 +0100

    Removed build content

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.tsx',
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
